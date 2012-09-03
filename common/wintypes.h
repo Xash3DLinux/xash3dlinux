67d8ae8f525b8f2900e5d3ad7f9063f57d13a8f2
fire64@yandex.ru <fire64@yandex.ru>
2012-09-03 15:30:25 +0400

    Porting the game launcher.

---
+++ common/wintypes.h
@@ -0,0 +1,21 @@
+
+//defined windows data types
+typedef void * HINSTANCE;
+typedef void * HMODULE;
+typedef void * FARPROC;
+typedef const char* LPCSTR;
+
+typedef unsigned char byte;
+typedef int BOOL;
+
+typedef unsigned long DWORD;
+typedef unsigned short WORD;
+
+typedef char CHAR;
+typedef CHAR *LPSTR;
+typedef unsigned char BYTE;
+
+typedef void *LPVOID;
+typedef void *PVOID;
+typedef PVOID HANDLE;
+typedef HANDLE HWND;
--- game_launch/game.cpp
+++ game_launch/game.cpp
@@ -13,7 +13,19 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 */
 
+#ifdef _WIN32
 #include <windows.h>
+#else
+#include "recdefs.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dlfcn.h>
+
+//X Windows
+#include <Xm/Xm.h>
+#include <Xm/PushB.h>
+#endif
+
 
 #define GAME_PATH	"valve"	// default dir to start from
 
@@ -26,14 +38,42 @@ pfnShutdown Host_Shutdown = NULL;
 char szGameDir[128]; // safe place to keep gamedir
 HINSTANCE	hEngine;
 
+#ifndef _WIN32
+int MessageBoxLinux( const char *lpText, const char *lpCaption )
+{
+	Widget top_wid, button;
+	XtAppContext  app;
+
+	int argc2 = 0;
+	char **argv2 = NULL;
+
+	top_wid = XtVaAppInitialize(&app, (char *)lpCaption, NULL, 0, &argc2, argv2, NULL, NULL);
+
+	button = XmCreatePushButton(top_wid, (char *)lpText, NULL, 0);
+
+	/* tell Xt to manage button */
+	XtManageChild(button);
+
+	XtRealizeWidget(top_wid); /* display widget hierarchy */
+	XtAppMainLoop(app); /* enter processing loop */
+
+	return 1;
+}
+#endif
+
 void Sys_Error( const char *errorstring )
 {
+#ifdef _WIN32
 	MessageBox( NULL, errorstring, "Xash Error", MB_OK|MB_SETFOREGROUND|MB_ICONSTOP );
+#else
+	MessageBoxLinux( errorstring, "Xash Error" );
+#endif
 	exit( 1 );
 }
 
 void Sys_LoadEngine( void )
 {
+#ifdef _WIN32
 	if(( hEngine = LoadLibrary( "xash.dll" )) == NULL )
 	{
 		Sys_Error( "Unable to load the xash.dll" );
@@ -46,12 +86,32 @@ void Sys_LoadEngine( void )
 
 	// this is non-fatal for us but change game will not working
 	Host_Shutdown = (pfnShutdown)GetProcAddress( hEngine, "Host_Shutdown" );
+#else
+	if(( hEngine = dlopen( "xash.so", RTLD_NOW )) == NULL )
+	{
+		Sys_Error( "Unable to load the xash.so" );
+	}
+
+	if(( Host_Main = (pfnInit)dlsym( hEngine, "Host_Main" )) == NULL )
+	{
+		Sys_Error( "xash.dll missed 'Host_Main' export" );
+	}
+
+	// this is non-fatal for us but change game will not working
+	Host_Shutdown = (pfnShutdown)dlsym( hEngine, "Host_Shutdown" );
+#endif
 }
 
 void Sys_UnloadEngine( void )
 {
+
 	if( Host_Shutdown ) Host_Shutdown( );
+
+#ifdef _WIN32
 	if( hEngine ) FreeLibrary( hEngine );
+#else
+	if( hEngine ) dlclose( hEngine );
+#endif
 }
 
 void Sys_ChangeGame( const char *progname )
@@ -66,7 +126,11 @@ void Sys_ChangeGame( const char *progname )
 	Host_Main( szGameDir, TRUE, ( Host_Shutdown != NULL ) ? Sys_ChangeGame : NULL );
 }
 
+#ifdef _WIN32
 int APIENTRY WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow )
+#else
+int main(int argc, char *argv[])
+#endif
 {
 	Sys_LoadEngine();
 
